801dd32b36f0054cac294efb4e9aed0f1ca351b2
Prabhu-H <prabhuharikesavan@gmail.com>
2021-12-07 17:06:31 +0530

    Updated Pincode

--- src/org/company/CompanyInfo.java
+++ src/org/company/CompanyInfo.java
@@ -10,10 +10,14 @@ public class CompanyInfo {
 	public void companyAddress() {
 		System.out.println("Chennai");
 	}
+	public void companyPincode() {
+		System.out.println("560067");
+	}
 	public static void main(String[] args) {
 		CompanyInfo details = new CompanyInfo();
 		details.companyName();
 		details.companyId();
 		details.companyAddress();
+		details.companyPincode();
 	}
 }
